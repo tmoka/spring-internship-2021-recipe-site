0977e10e2194d90b8b92fcfc3eaf004da9b748ce
tmoka0476 <knowledzr+alt@gmail.com>
2021-03-22 21:31:56 +0900

    レシピの型を定義

---
+++ constants/types.tsx
@@ -0,0 +1,28 @@
+// 個々のレシピを表す型
+export interface RecipeType {
+    id: number;
+    title: string;
+    description: string;
+    image_url: string;
+    author: authorType;
+    published_at: string;
+    ingredients: ingredientsType;
+    related_recipes: number[];
+}
+
+export type authorType = {
+    user_name: string
+}
+
+export type stepsType = {
+    steps: string[];
+}
+
+export type ingredientType = {
+    name: string;
+    quantity: string;
+}
+
+export type ingredientsType = {
+    ingredients: ingredientType[];
+}

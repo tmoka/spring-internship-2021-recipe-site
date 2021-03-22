dd767d7f65af7ee657a9caddfb7c40fdb480a6b2
tmoka0476 <knowledzr+alt@gmail.com>
2021-03-22 23:10:02 +0900

    prettier導入

--- constants/types.tsx
+++ constants/types.tsx
@@ -1,28 +1,28 @@
 // 個々のレシピを表す型
 export interface RecipeType {
-    id: number;
-    title: string;
-    description: string;
-    image_url: string;
-    author: authorType;
-    published_at: string;
-    ingredients: ingredientsType;
-    related_recipes: number[];
+  id: number;
+  title: string;
+  description: string;
+  image_url: string;
+  author: authorType;
+  published_at: string;
+  ingredients: ingredientsType;
+  related_recipes: number[];
 }
 
 export type authorType = {
-    user_name: string
-}
+  user_name: string;
+};
 
 export type stepsType = {
-    steps: string[];
-}
+  steps: string[];
+};
 
 export type ingredientType = {
-    name: string;
-    quantity: string;
-}
+  name: string;
+  quantity: string;
+};
 
 export type ingredientsType = {
-    ingredients: ingredientType[];
-}
+  ingredients: ingredientType[];
+};

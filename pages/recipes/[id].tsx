a379eb8fbdac159af5bc20b16b8686c2aa8c0da1
tmoka0476 <knowledzr+alt@gmail.com>
2021-03-23 17:41:29 +0900

    修正

--- pages/recipes/[id].tsx
+++ pages/recipes/[id].tsx
@@ -1,7 +1,7 @@
 import { FC } from "react"
 import { useRouter } from "next/router"
 import { RecipeType } from "../../constants/types"
-import Head from "next/Head"
+import Head from "next/head"
 
 const RecipePage: FC<RecipeType> = (props: RecipeType) => {
   const router = useRouter()

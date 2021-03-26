286a5a277a924debd990fd5cb4ad23534fc90e54
tmoka0476 <knowledzr+alt@gmail.com>
2021-03-26 13:06:55 +0900

    bundler-analyzer導入

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+module.exports = withBundleAnalyzer({})
+
 module.exports = {
     images: {
       domains: ['img.cpcdn.com', 'staff-recipes-images-prod.s3.ap-northeast-1.amazonaws.com'],
     },
-  }
+}
